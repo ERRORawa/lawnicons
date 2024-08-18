7f1e0bcdad4bb497b511eace8995c82094cf1322
ERRORawa <95691967+ERRORawa@users.noreply.github.com>
2024-08-19 03:00:51 +0800

    增加4个图标

---
+++ app/src/debug/res/make.sh
@@ -0,0 +1,13 @@
+cd drawable
+for f in `ls`
+do
+    sed "s/\(rtHeight=\"192\"\>\)/rtHeight=\"192\"\n\    android:tint=\"\@color\/primaryForeground\"\>/g" $f > ../finish/$f
+    sed "s/\(res\/android\"\>\)/res\/android\"\n\    android:tint=\"\@color\/primaryForeground\"\>/g" $f > ../finish/$f
+    rm $f
+    cd ../finish
+    i=`echo "${f%_foreground*}"`
+    echo $i
+    touch $i.xml
+    sed "s/\(nul\)/$i/g" ../bool.xml > $i.xml
+    cd ../drawable
+done
